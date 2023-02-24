b879a8ad79dc74be82d70c8a25ccf976a7c5d816
Ultramegasati <ultramegasati@hotmail.com>
2023-02-23 21:16:47 -0300

    Executando via terminal

---
+++ Conhecendo_A_Linguagem_Java/src/ExecutandoPeloTerminal/ExecutandoViaTerminal.java
@@ -0,0 +1,19 @@
+package ExecutandoPeloTerminal;
+
+public class ExecutandoViaTerminal {
+
+	public static void main(String[] args) {
+
+		System.out.println("Fui executado pelo terminal");
+
+	}
+
+}
+/*
+ * arquivos compilados ficam na pasta bin .class;
+ * entrar na pasta bin pelo MS-DOSe achar o arquivo
+ * desejavél .class;
+ * para executar  java nomeclasse   //sem o .class
+ * 
+ * 
+ */
